003251592820ffd45f58c61f29cd4c191d913da9
Briana Harper <williamsbriana2022@gmail.com>
2024-06-05 21:37:55 -0400

    Add behavior

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-     <header className="App-header text-center">Dictionary</header>
+      <div className="container">
+        <header className="App-header text-center">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer text-center"><small>This project was built by Briana Harper</small></footer>
+      </div>
     </div>
   );
 }
