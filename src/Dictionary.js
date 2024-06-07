3d6c1948cff3fc41690151907cfb3b8cb83d0495
Briana Harper <williamsbriana2022@gmail.com>
2024-06-07 15:54:45 -0400

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    const [keyword, setKeyword] = useState("")
+    let [keyword, setKeyword] = useState("")
+
+    function handleResponse(response) {
+        console.log(response.data);
+    }
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`)
+        
+
+        let apiKey = "a94b5f688aoee508a9b7fca6t23274ef"
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
+
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event) {
