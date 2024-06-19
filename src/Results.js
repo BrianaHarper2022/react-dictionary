d3d121141b99da4662a1f7cdde606bffc585a2a9
Briana Harper <williamsbriana2022@gmail.com>
2024-06-19 17:19:03 -0400

    Rendered Phonetics

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,11 @@ export default function Results(props) {
     if (props.results) {
         return (
           <div className="Results">
-            <h2>{props.results.word}</h2>
+            <section>
+              <h2>{props.results.word}</h2>
+              <p>/{props.results.phonetic}/</p>
+            </section>
+
             {props.results.meanings.map(function (meanings, index) {
                 return (
                 <div key={index}>
