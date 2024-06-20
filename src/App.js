e5051d08acd0f218811e40c26429fca88bfec369
Briana Harper <williamsbriana2022@gmail.com>
2024-06-19 21:48:21 -0400

    Added more styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header text-center">Dictionary</header>
         <main>
-          <Dictionary defaultKeyword="love"/>
+          <Dictionary defaultKeyword="developer"/>
         </main>
         <footer className="app-footer text-center">This project was built by <a href="https://github.com/BrianaHarper2022" target="_blank"
             rel="noopener noreferrer">Briana Harper</a> and is <a href="https://github.com/BrianaHarper2022?tab=repositories" target="_blank" rel="noopener noreferrer">open-sourced on Github</a> and <a href="https://dictionary-react-addon.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a></footer>
