a613d2ef85d04ed6899626359caf22ec0be8113a
Briana Harper <williamsbriana2022@gmail.com>
2024-06-19 21:33:52 -0400

    Updated footer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default function App() {
         <main>
           <Dictionary defaultKeyword="love"/>
         </main>
-        <footer className="app-footer text-center"><small>This project was built by Briana Harper</small></footer>
+        <footer className="app-footer text-center">This project was built by <a href="https://github.com/BrianaHarper2022" target="_blank"
+            rel="noopener noreferrer">Briana Harper</a> and is <a href="https://github.com/BrianaHarper2022?tab=repositories" target="_blank" rel="noopener noreferrer">open-sourced on Github</a> and <a href="https://dictionary-react-addon.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a></footer>
       </div>
     </div>
   );
