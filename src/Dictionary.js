b2d063f8590ce1ed615ec83cff9fa622143b25f4
Briana Harper <williamsbriana2022@gmail.com>
2024-06-20 15:54:45 -0400

    Integrated SheCodes Image API and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,38 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
     const [keyword, setKeyword] = useState(props.defaultKeyword)
     const [results, setResults] = useState(null);
     const [loaded, setLoaded] = useState(false);
- 
+    const [photos, setPhotos] = useState([]);
 
+    function handleImages(response) {
+        setPhotos(response.data.photos);
+    }
+ 
     function handleResponse(response) {
         setResults(response.data)
+        const imagesApiKey = "a94b5f688aoee508a9b7fca6t23274ef"
+        const imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${response.data.word}&key=${imagesApiKey}`
+        axios
+            .get(imagesApiUrl, { headers: { Authorization: `Bearer ${imagesApiKey}` } })
+            .then(handleImages);
+    }
+
+    function load() {
+        setLoaded(true);
+        search();
     }
 
     function search() {
         let apiKey = "a94b5f688aoee508a9b7fca6t23274ef"
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
         axios.get(apiUrl).then(handleResponse);
+
     }
 
     function handleSubmit(event) {
@@ -27,11 +43,7 @@ export default function Dictionary(props) {
     function handleKeywordChange(event) {
         setKeyword(event.target.value)
     }
-    function load() {
-        setLoaded(true);
-        search();
-    }
-
+   
    if (loaded) {
         return (
             <div className="Dictionary">
@@ -45,6 +57,7 @@ export default function Dictionary(props) {
                     </div>
                 </section>
                 <Results results={results} />
+                <Photos photos={photos} />
             </div>
         );
     } else {
